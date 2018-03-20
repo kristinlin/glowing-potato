cc9e81d930acc3681da9a5d76dd1a2d6917feb72
Kristin Lin <klin11@stuy.edu>
2018-03-20 00:01:14 -0400

    axes onnnn

--- plot.js
+++ plot.js
@@ -1,6 +1,7 @@
 
 var svg = document.getElementById("place");
-var data = [11.84, 28.16, 32, 12.16, 84.48, 36.8, 37.44, 37.76]; 
+var data = [11.84, 28.16, 32, 12.16, 84.48, 36.8, 37.44, 37.76];
+var planet = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 
 var create = function() {
     var d;
@@ -14,22 +15,55 @@ var create = function() {
 }
 
 
-var linearScale = d3.scaleLinear()
-    .domain([0, 10])
-    .range([0, 500]);
+var xlinearScale = d3.scaleLinear()
+    .domain([-1, 9])
+    .range([0, 900]);
+var ylinearScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([260, 0]);
 
 var plot = function() {
     var circles = d3.selectAll("circle");
     circles.data(data);
     circles.attr("cx",
 		 function(d, i) {
-		     return linearScale(i) + 10;
+		     return xlinearScale(i);
 		 });
     circles.attr("cy",
 		 function(d) {
-		     return d;
+		     return ylinearScale(d);
 		 });
+}
+
+var axes = function() {
+    var x_axis = d3.axisBottom()
+	.scale(xlinearScale)
+    	.tickFormat(
+	    function(d, i){
+		return planet[i-1];
+	    }
+	);
+    var y_axis = d3.axisLeft()
+	.scale(ylinearScale);
+    
+    var d3_svg = d3.select("svg");
+    d3_svg.append("g")
+	.attr("transform", "translate(0, 260)")
+	.call(x_axis);
+    d3_svg.append("text")
+	.attr("transform", "translate(450, 300)")
+	.style("text-anchor", "middle")
+	.text("Planets");
+    d3_svg.append("g")
+    	.attr("transform", "translate(50, 0)")
+	.call(y_axis);
+    d3_svg.append("text")
+        .attr("transform", "translate(15, 125)rotate(-90)")
+	.style("text-anchor", "middle")
+	.text("Acceleration of Gravity");
+    
 }
 
 create();
 plot();
+axes();
